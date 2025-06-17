db93ae9eaafc4e2188e7ca77f7819494d711126f
per1Peteia <justusmanthey@googlemail.com>
2025-06-17 09:00:25 +0200

    conversation list using types.Parts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -18,9 +19,13 @@ def main():
         print(f'Error: {e}')
         sys.exit(1)
 
+    conversation = [
+        types.Content(role="user", parts=[types.Part(text=user_message)]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_message
+        contents=conversation
     )
 
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
