8e495283a51a90d9f7f67e47005ff517c39f84d0
per1Peteia <justusmanthey@googlemail.com>
2025-06-17 08:44:19 +0200

    user message from command line arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,9 +8,19 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    try:
+        user_message = sys.argv[1]
+    except IndexError:
+        print('Usage: python main.py <"user message">')
+        sys.exit(1)
+    except Exception as e:
+        print(f'Error: {e}')
+        sys.exit(1)
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=user_message
     )
 
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
