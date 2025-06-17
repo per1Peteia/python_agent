6c2c8f23785c353d68efcd7b9c554217274c0ce0
per1Peteia <justusmanthey@googlemail.com>
2025-06-17 09:34:58 +0200

    verbose flag

--- main.py
+++ main.py
@@ -10,14 +10,7 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    try:
-        user_message = sys.argv[1]
-    except IndexError:
-        print('Usage: python main.py <"user message">')
-        sys.exit(1)
-    except Exception as e:
-        print(f'Error: {e}')
-        sys.exit(1)
+    user_message, options = parse_commandline()
 
     conversation = [
         types.Content(role="user", parts=[types.Part(text=user_message)]),
@@ -28,10 +21,38 @@ def main():
         contents=conversation
     )
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
+    if options['verbose']:
+        print("User prompt:", user_message)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(response.text)
+        sys.exit(0)
+
     print(response.text)
+    sys.exit(0)
+
+
+def parse_commandline():
+    try:
+        user_message = sys.argv[1]
+        options = {"verbose": False}
+
+        if len(sys.argv) == 2:
+            return user_message, options
+
+        flag = sys.argv[2]
+        if flag == "--verbose":
+            options['verbose'] = True
+
+        return (user_message, options)
+    except IndexError as e:
+        print('Usage: python main.py <"user message">')
+        print(f'{e}')
+        sys.exit(1)
+    except Exception as e:
+        print(f'Error: {e}')
+        sys.exit(1)
 
 
 if __name__ == "__main__":
