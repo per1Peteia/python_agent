f974b11adf567ad96b86bb9326e5b6b89cbedc87
per1Peteia <justusmanthey@googlemail.com>
2025-06-19 09:50:50 +0200

    write file tool

---
+++ tools/write_file.py
@@ -0,0 +1,17 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    abs_wd = os.path.abspath(working_directory)
+    abs_target_file = os.path.abspath(
+        os.path.join(working_directory, file_path))
+    if not abs_target_file.startswith(abs_wd):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+    try:
+        with open(abs_target_file, 'w') as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error writing file: {e}'
